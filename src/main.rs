168cfca7b63a19ab502c9ed54a8d2a8ff4c55ae2
fluidvanadium <fluidvanadium@zingolabs.org>
2025-01-20 10:27:19 +0000

    new algo

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,17 @@ use std::io::Write;
 use std::path::Path;
 
 async fn app() -> Result<u64, Error> {
+    let n = 256;
+    let dest_filename = format!("./draftmancer-sealed{n}.txt");
+
     let source_path = Path::new("./dmbase.txt");
-    let dest_path = Path::new("./draftmancer-sealed99.txt");
+    let dest_path = Path::new(dest_filename.as_str());
 
     let mut source_file = File::open(source_path).unwrap();
     let mut source_data = String::new();
     source_file.read_to_string(&mut source_data).unwrap();
+
+    source_data = source_data.replace("99", n.to_string().as_str());
 
     let mut dest_file = File::create(dest_path).unwrap();
     dest_file.write_all(source_data.as_bytes()).unwrap();
