e1674a20e188e60bf5ea9ef6a3ebe47cdee0c44a
fluidvanadium <fluidvanadium@zingolabs.org>
2026-02-19 20:28:27 +0000

    cleaning up

--- src/main.rs
+++ src/main.rs
@@ -275,197 +275,203 @@ async fn main() {
                 // conspiracy.clone(),
             ]),
         ),
-        // 14 card pickup decks
-        (
-            "./tdm.txt".to_string(),
-            Query::And(vec![set("TDM"), not(type_line("basic"))]),
-        ),
-        (
-            "./dft.txt".to_string(),
-            Query::And(vec![set("DFT"), not(type_line("basic"))]),
-        ),
-        (
-            "./fdn.txt".to_string(),
-            Query::And(vec![set("FDN"), not(type_line("basic"))]),
-        ),
-        (
-            "./apc.txt".to_string(),
-            Query::And(vec![set("APC"), not(type_line("basic"))]),
-        ),
-        (
-            "./dsk.txt".to_string(),
-            Query::And(vec![set("DSK"), not(type_line("basic"))]),
-        ),
-        (
-            "./block_rav.txt".to_string(),
-            Query::And(vec![block("RAV"), not(type_line("basic"))]),
-        ),
     ];
-    let format = [
-        (
-            "./fin-common.txt".to_string(),
-            Query::And(vec![
-                set("FIN"),
-                not(type_line("basic")),
-                rarity(Rarity::Common),
-            ]),
-        ),
-        (
-            "./fin-uncommon.txt".to_string(),
-            Query::And(vec![
-                set("FIN"),
-                not(type_line("basic")),
-                rarity(Rarity::Uncommon),
-            ]),
-        ),
-        (
-            "./fin-rare.txt".to_string(),
-            Query::And(vec![
-                set("FIN"),
-                not(type_line("basic")),
-                rarity(Rarity::Rare),
-            ]),
-        ),
-        (
-            "./fin-mythic.txt".to_string(),
-            Query::And(vec![
-                set("FIN"),
-                not(type_line("basic")),
-                rarity(Rarity::Mythic),
-            ]),
-        ),
-    ];
-    let format = [
-        (
-            "./commanders.txt".to_string(),
-            Query::And(vec![
-                type_line("legendary"),
-                type_line("creature"),
-            ]),
-        ),
-    ];
-    let formargt = [
-        (
-            "./for-subset-draft-Bonus.txt".to_string(),
-            Query::Or(vec![
-                Query::And(vec![rarity(Rarity::Bonus), rebalanced.clone()]),
-                Query::And(vec![
-                    rarity(Rarity::Bonus),
-                    vintage_taste_ban.clone(),
-                    not(basics.clone()),
-                    not(meld_duds.clone()),
-                    not(unfun.clone()),
-                    //
-                    not(commander_synergy.clone()),
-                    // not(draft_involved.clone()),
-                ]),
-                conspiracy.clone(),
-            ]),
-        ),
-        (
-            "./for-subset-draft-Mythic.txt".to_string(),
-            Query::Or(vec![
-                Query::And(vec![rarity(Rarity::Mythic), rebalanced.clone()]),
-                Query::And(vec![
-                    rarity(Rarity::Mythic),
-                    vintage_taste_ban.clone(),
-                    not(basics.clone()),
-                    not(meld_duds.clone()),
-                    not(unfun.clone()),
-                    //
-                    not(commander_synergy.clone()),
-                    // not(draft_involved.clone()),
-                ]),
-                conspiracy.clone(),
-            ]),
-        ),
-        (
-            "./for-subset-draft-Special.txt".to_string(),
-            Query::Or(vec![
-                Query::And(vec![rarity(Rarity::Special), rebalanced.clone()]),
-                Query::And(vec![
-                    rarity(Rarity::Special),
-                    vintage_taste_ban.clone(),
-                    not(basics.clone()),
-                    not(meld_duds.clone()),
-                    not(unfun.clone()),
-                    //
-                    not(commander_synergy.clone()),
-                    // not(draft_involved.clone()),
-                ]),
-                conspiracy.clone(),
-            ]),
-        ),
-        (
-            "./for-subset-draft-Rare.txt".to_string(),
-            Query::Or(vec![
-                Query::And(vec![rarity(Rarity::Rare), rebalanced.clone()]),
-                Query::And(vec![
-                    rarity(Rarity::Rare),
-                    vintage_taste_ban.clone(),
-                    not(basics.clone()),
-                    not(meld_duds.clone()),
-                    not(unfun.clone()),
-                    //
-                    not(commander_synergy.clone()),
-                    // not(draft_involved.clone()),
-                ]),
-                conspiracy.clone(),
-            ]),
-        ),
-        (
-            "./for-subset-draft-Uncommon.txt".to_string(),
-            Query::Or(vec![
-                Query::And(vec![rarity(Rarity::Uncommon), rebalanced.clone()]),
-                Query::And(vec![
-                    rarity(Rarity::Uncommon),
-                    vintage_taste_ban.clone(),
-                    not(basics.clone()),
-                    not(meld_duds.clone()),
-                    not(unfun.clone()),
-                    //
-                    not(commander_synergy.clone()),
-                    // not(draft_involved.clone()),
-                ]),
-                conspiracy.clone(),
-            ]),
-        ),
-        (
-            "./for-subset-draft-Common.txt".to_string(),
-            Query::Or(vec![
-                Query::And(vec![rarity(Rarity::Common), rebalanced.clone()]),
-                Query::And(vec![
-                    rarity(Rarity::Common),
-                    vintage_taste_ban.clone(),
-                    not(basics.clone()),
-                    not(meld_duds.clone()),
-                    not(unfun.clone()),
-                    //
-                    not(commander_synergy.clone()),
-                    // not(draft_involved.clone()),
-                ]),
-                conspiracy.clone(),
-            ]),
-        ),
-    ];
+    // 14 card pickup decks
     for (destination_filename, der_query) in format.iter() {
-        // let index = 6;
-        // let (destination_filename, der_query) = format[index].clone();
-        let query = &der_query;
-        let (list, report) = query_to_draftmancer_list(query).await;
+        write_query_to_file(destination_filename, der_query);
+    }
+}
 
-        let list_path_name = "lists/".to_string() + destination_filename.as_str();
-        let list_path = Path::new(list_path_name.as_str());
-        let report_path_name = "reports/".to_string() + destination_filename.as_str();
-        let report_path = Path::new(report_path_name.as_str());
-        let mut list_file = File::create(list_path).unwrap();
-        let mut report_file = File::create(report_path).unwrap();
+async fn write_query_to_file(destination_filename: &str, der_query: &Query) {
+    // let index = 6;
+    // let (destination_filename, der_query) = format[index].clone();
+    let query = &der_query;
+    let (list, report) = query_to_draftmancer_list(query).await;
 
-        list_file
-            .write_all(list.as_bytes())
-            .expect("Unable to write data");
-        report_file
-            .write_all(report.to_string().as_bytes())
-            .expect("Unable to write data");
+    let list_path_name = "lists/".to_string() + destination_filename;
+    let list_path = Path::new(list_path_name.as_str());
+    let report_path_name = "reports/".to_string() + destination_filename;
+    let report_path = Path::new(report_path_name.as_str());
+    let mut list_file = File::create(list_path).unwrap();
+    let mut report_file = File::create(report_path).unwrap();
+
+    list_file
+        .write_all(list.as_bytes())
+        .expect("Unable to write data");
+    report_file
+        .write_all(report.to_string().as_bytes())
+        .expect("Unable to write data");
+}
+
+mod old_formats {
+    async fn old_formats() {
+        [
+            (
+                "./tdm.txt".to_string(),
+                Query::And(vec![set("TDM"), not(type_line("basic"))]),
+            ),
+            (
+                "./dft.txt".to_string(),
+                Query::And(vec![set("DFT"), not(type_line("basic"))]),
+            ),
+            (
+                "./fdn.txt".to_string(),
+                Query::And(vec![set("FDN"), not(type_line("basic"))]),
+            ),
+            (
+                "./apc.txt".to_string(),
+                Query::And(vec![set("APC"), not(type_line("basic"))]),
+            ),
+            (
+                "./dsk.txt".to_string(),
+                Query::And(vec![set("DSK"), not(type_line("basic"))]),
+            ),
+            (
+                "./block_rav.txt".to_string(),
+                Query::And(vec![block("RAV"), not(type_line("basic"))]),
+            ),
+        ];
+        let format = [
+            (
+                "./fin-common.txt".to_string(),
+                Query::And(vec![
+                    set("FIN"),
+                    not(type_line("basic")),
+                    rarity(Rarity::Common),
+                ]),
+            ),
+            (
+                "./fin-uncommon.txt".to_string(),
+                Query::And(vec![
+                    set("FIN"),
+                    not(type_line("basic")),
+                    rarity(Rarity::Uncommon),
+                ]),
+            ),
+            (
+                "./fin-rare.txt".to_string(),
+                Query::And(vec![
+                    set("FIN"),
+                    not(type_line("basic")),
+                    rarity(Rarity::Rare),
+                ]),
+            ),
+            (
+                "./fin-mythic.txt".to_string(),
+                Query::And(vec![
+                    set("FIN"),
+                    not(type_line("basic")),
+                    rarity(Rarity::Mythic),
+                ]),
+            ),
+        ];
+        let format = [(
+            "./commanders.txt".to_string(),
+            Query::And(vec![type_line("legendary"), type_line("creature")]),
+        )];
+        // let formargt = [
+        //     (
+        //         "./for-subset-draft-Bonus.txt".to_string(),
+        //         Query::Or(vec![
+        //             Query::And(vec![rarity(Rarity::Bonus), rebalanced.clone()]),
+        //             Query::And(vec![
+        //                 rarity(Rarity::Bonus),
+        //                 vintage_taste_ban.clone(),
+        //                 not(basics.clone()),
+        //                 not(meld_duds.clone()),
+        //                 not(unfun.clone()),
+        //                 //
+        //                 not(commander_synergy.clone()),
+        //                 // not(draft_involved.clone()),
+        //             ]),
+        //             conspiracy.clone(),
+        //         ]),
+        //     ),
+        //     (
+        //         "./for-subset-draft-Mythic.txt".to_string(),
+        //         Query::Or(vec![
+        //             Query::And(vec![rarity(Rarity::Mythic), rebalanced.clone()]),
+        //             Query::And(vec![
+        //                 rarity(Rarity::Mythic),
+        //                 vintage_taste_ban.clone(),
+        //                 not(basics.clone()),
+        //                 not(meld_duds.clone()),
+        //                 not(unfun.clone()),
+        //                 //
+        //                 not(commander_synergy.clone()),
+        //                 // not(draft_involved.clone()),
+        //             ]),
+        //             conspiracy.clone(),
+        //         ]),
+        //     ),
+        //     (
+        //         "./for-subset-draft-Special.txt".to_string(),
+        //         Query::Or(vec![
+        //             Query::And(vec![rarity(Rarity::Special), rebalanced.clone()]),
+        //             Query::And(vec![
+        //                 rarity(Rarity::Special),
+        //                 vintage_taste_ban.clone(),
+        //                 not(basics.clone()),
+        //                 not(meld_duds.clone()),
+        //                 not(unfun.clone()),
+        //                 //
+        //                 not(commander_synergy.clone()),
+        //                 // not(draft_involved.clone()),
+        //             ]),
+        //             conspiracy.clone(),
+        //         ]),
+        //     ),
+        //     (
+        //         "./for-subset-draft-Rare.txt".to_string(),
+        //         Query::Or(vec![
+        //             Query::And(vec![rarity(Rarity::Rare), rebalanced.clone()]),
+        //             Query::And(vec![
+        //                 rarity(Rarity::Rare),
+        //                 vintage_taste_ban.clone(),
+        //                 not(basics.clone()),
+        //                 not(meld_duds.clone()),
+        //                 not(unfun.clone()),
+        //                 //
+        //                 not(commander_synergy.clone()),
+        //                 // not(draft_involved.clone()),
+        //             ]),
+        //             conspiracy.clone(),
+        //         ]),
+        //     ),
+        //     (
+        //         "./for-subset-draft-Uncommon.txt".to_string(),
+        //         Query::Or(vec![
+        //             Query::And(vec![rarity(Rarity::Uncommon), rebalanced.clone()]),
+        //             Query::And(vec![
+        //                 rarity(Rarity::Uncommon),
+        //                 vintage_taste_ban.clone(),
+        //                 not(basics.clone()),
+        //                 not(meld_duds.clone()),
+        //                 not(unfun.clone()),
+        //                 //
+        //                 not(commander_synergy.clone()),
+        //                 // not(draft_involved.clone()),
+        //             ]),
+        //             conspiracy.clone(),
+        //         ]),
+        //     ),
+        //     (
+        //         "./for-subset-draft-Common.txt".to_string(),
+        //         Query::Or(vec![
+        //             Query::And(vec![rarity(Rarity::Common), rebalanced.clone()]),
+        //             Query::And(vec![
+        //                 rarity(Rarity::Common),
+        //                 vintage_taste_ban.clone(),
+        //                 not(basics.clone()),
+        //                 not(meld_duds.clone()),
+        //                 not(unfun.clone()),
+        //                 //
+        //                 not(commander_synergy.clone()),
+        //                 // not(draft_involved.clone()),
+        //             ]),
+        //             conspiracy.clone(),
+        //         ]),
+        //     ),
+        // ];
     }
 }
